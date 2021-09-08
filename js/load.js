f1de0f3501ddd0941698a81eec173337ae154e36
Sahil More <model64321@gmail.com>
2021-09-08 11:33:40 +0530

    PWA Done

--- js/load.js
+++ js/load.js
@@ -5,7 +5,6 @@ window.addEventListener("load", function() {
     function removeFadeOut(el, speed) {
         var seconds = speed / 1000;
         el.style.transition = "opacity " + seconds + "s ease";
-
         el.style.opacity = 0;
         setTimeout(function() {
             el.parentNode.removeChild(el);
